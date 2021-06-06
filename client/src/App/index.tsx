e76b366ead54e285d6495a133cd33491feba3af5
pradeep-selva <pradeepselvaraju@gmail.com>
2021-06-07 00:28:30 +0530

    fetching contract details for display

--- client/src/App/index.tsx
+++ client/src/App/index.tsx
@@ -1,18 +1,45 @@
 import React from "react";
 import "./index.css";
-import lottery from "../Utils";
+import { Lottery, web3 } from "../Utils";
 
-class App extends React.Component {
+interface IState {
+  manager: string;
+  participants: Array<string>;
+  balance: string;
+}
+
+class App extends React.Component<any, IState> {
+  constructor(props: any) {
+    super(props);
+
+    this.state = {
+      manager: "",
+      participants: [],
+      balance: ""
+    };
+  }
+
   async componentDidMount() {
-    const manager = await lottery.methods?.getManager()?.call();
-    console.log("manager", manager);
+    const participants = await Lottery.methods?.getParticipants()?.call();
+    const balance = await web3.eth.getBalance(Lottery.options.address);
+    const manager = await Lottery.methods?.getManager()?.call();
+
+    this.setState({ manager, participants, balance });
   }
 
   render() {
+    const { manager, participants, balance } = this.state;
     return (
       <div className='App'>
         <header className='App-header'>
           <h1>Lottery</h1>
+          {!!manager && (
+            <h6>
+              This contract is managed by {manager}. <br /> There are currently{" "}
+              {participants.length} participants competing for a prize pool of{" "}
+              {balance} ether.
+            </h6>
+          )}
         </header>
       </div>
     );
---
+++ client/src/Utils/web3.ts
@@ -0,0 +1,29 @@
+import Web3 from "web3";
+
+let web3: Web3;
+
+const Window = window as Window &
+  typeof globalThis & { ethereum: any; web3: Web3 };
+
+if (typeof Window.ethereum !== "undefined") {
+  web3 = new Web3(Window.ethereum);
+
+  if (typeof Window.ethereum.autoRefreshOnNetworkChange !== "undefined") {
+    Window.ethereum.autoRefreshOnNetworkChange = false;
+  }
+
+  Window.ethereum.on("chainChanged", () => {
+    document.location.reload();
+  });
+
+  Window.ethereum
+    .enable()
+    .then(console.log)
+    .catch(() =>
+      alert("This application requires user approval to function correctly!")
+    );
+} else {
+  web3 = new Web3(Window.web3.currentProvider);
+}
+
+export default web3;
