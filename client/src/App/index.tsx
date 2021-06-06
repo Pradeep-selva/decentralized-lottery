f6c2f5bad468a45e538d876618e22ecdeabfd948
pradeep-selva <pradeepselvaraju@gmail.com>
2021-06-06 17:26:27 +0530

    initialised client

---
+++ client/src/App/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./index.css";
+
+function App() {
+  return (
+    <div className='App'>
+      <header className='App-header'>
+        <h1>Lottery</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
