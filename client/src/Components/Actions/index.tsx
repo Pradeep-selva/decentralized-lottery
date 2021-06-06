a208b73d2c89f8911c418b4a13e019e11b8524b5
pradeep-selva <pradeepselvaraju@gmail.com>
2021-06-07 01:55:23 +0530

    entering contest functionality

--- client/src/Components/Actions/index.tsx
+++ client/src/Components/Actions/index.tsx
@@ -1,12 +1,33 @@
 import React, { useState } from "react";
 import "./index.css";
-// import { Lottery } from "../../Utils";
+import { Lottery, web3 } from "../../Utils";
 
 const Actions = () => {
-  const [ether, setEther] = useState(0);
+  const [ether, setEther] = useState("0");
+  const [message, setMessage] = useState("");
+
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    setMessage("Waiting for transaction confirmation...");
+
+    try {
+      const accounts = await web3.eth.getAccounts();
+      await Lottery.methods
+        ?.enterContest()
+        ?.send({ from: accounts[0], value: web3.utils.toWei(ether, "ether") });
+
+      setMessage("Joined successfully!");
+    } catch (err) {
+      console.log(err);
+      setMessage("Could not join successfully.");
+    }
+  };
 
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
+      <h4>
+        <u>Test your luck</u>
+      </h4>
       <div className={"form-control"}>
         <label style={{ fontSize: "1.5rem" }}>
           <b>Ether (min: 0.001)</b>
@@ -14,12 +35,16 @@ const Actions = () => {
         <input
           className={"input"}
           type={"number"}
+          step={0.001}
           value={ether}
           style={{ marginTop: 10 }}
-          onChange={({ target: { value } }) => setEther(parseInt(value))}
+          onChange={({ target: { value } }) => setEther(value)}
         />
       </div>
-      <input type={"submit"} className={"submit"} />
+      <button type={"submit"} className={"submit"}>
+        <b>Join</b>
+      </button>
+      <h5>{message}</h5>
     </form>
   );
 };
